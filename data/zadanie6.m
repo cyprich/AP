9e5b6fee88f969132424143fcb87fc55ab6793b2
cyprich <cypooriginal@gmail.com>
2024-12-09 14:47:43 +0100

    zadanie 6 skoro done chyba prezentacia, vypocet chyb, adaptabilna MA() asi nie je dobre toto bol boj, a este bude

--- data/zadanie6.m
+++ data/zadanie6.m
@@ -3,105 +3,146 @@ clear; close all;
 import = readmatrix("matlabized_data2.csv");
 t_full = import(:, 1)';
 f_full = import(:, 2)';
+l = length(f_full);  % dlzka dat
 
-figure; hold on; grid on; xlim([1 124])
-plot(t_full, f_full, "-")
 
-% function fp = f_ma(f, n)  % funkcia na vypocet AR(n), parametre su (data, n), vrati hodnotu fp
-%     l = length(f);
-% 
-%     for i = 1:(n/2 + 1)
-%         fprintf("---%d---\n", l)
-%         disp(n/2+i)
-%         disp(l-(n/2-i))
-%         disp(n/2-i)
-%         disp(l-(n/2+i))
-% 
-%         b{i} = f(n/2+i: l-(n/2-i));
-%         b{i*2} = f((n/2-i): l-(n/2+i));
-%     end
-% 
-%     for i = 1:n
-%         B(i) = b{i};
-%     end
-% 
-%     c = (f*B')*(B*B')^(-1);
-%     fp = c*B;
-% 
-% end
+figure; hold on; grid on; xlim([1 124]); legend; title("Dáta - hodnoty akcií spoločnosti EA"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, "-", DisplayName="Dáta");
 
-% -------------------- MA(2) --------------------
-f = f_full(2: 123);
-ma_2_t = t_full(2: 123);
+% ----------------------- standardna MA ---------------------------
+% MA(2)
+for i = 1:l
+    if i == 1 || i == l
+        ma2_std(i) = NaN;
+    else
+        ma2_std(i) = (f_full(i-1) + f_full(i+1)) / 2;
+    end
+end
 
-b1 = f_full(1: 122);
-b2 = f_full(3: 124);
+
+% MA(4)
+for i = 1:l
+    if i <= 2 || i >= l-1
+        ma4_std(i) = NaN;
+    else
+        ma4_std(i) = (f_full(i-2) + f_full(i-1) + f_full(i+1) + f_full(i+2)) / 4;
+    end
+end
+
+% MA(6)
+for i = 1:l
+    if i <= 3 || i >= l-2
+        ma6_std(i) = NaN;
+    else
+        ma6_std(i) = (f_full(i-3) + f_full(i-2) + f_full(i-1) + f_full(i+1) + f_full(i+2) + f_full(i+3)) / 6;
+    end
+end
+
+figure; hold on; grid on; xlim([1 124]); title("Filtrácia pomocou štandardnej metódy MA(2)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, DisplayName="Dáta");
+plot(t_full, ma2_std, LineWidth=1.5, DisplayName="Štandardná MA(2)");
+
+figure; hold on; grid on; xlim([1 124]); legend; title("Filtrácia pomocou štandardnej metódy MA(4)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, DisplayName="Dáta");
+plot(t_full, ma2_std, DisplayName="Štandardná MA(2)");
+plot(t_full, ma4_std, LineWidth=1.5, DisplayName="Štandardná MA(4)");
+
+figure; hold on; grid on; xlim([1 124]); legend; title("Filtrácia pomocou štandardnej metódy MA(6)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, DisplayName="Dáta");
+plot(t_full, ma2_std, DisplayName="Štandardná MA(2)");
+plot(t_full, ma4_std, DisplayName="Štandardná MA(4)");
+plot(t_full, ma6_std, LineWidth=1.5, DisplayName="Štandardná MA(6)");
+
+% ----------------------- adaptabilna MA ---------------------------
+% MA(2)
+fz = f_full(2: l-1);
+b1 = f_full(3: l);
+b2 = f_full(1: l-2);
+
 B = [b1; b2];
+c = (fz * B') * (B * B')^(-1);
+ma2_adapt = c*B;
+fprintf("koeficienty ma2: %d\n", c)
 
-c = (f*B')*(B*B')^(-1);
-ma_2_std = c*B;
-
-% -------------------- MA(4) --------------------
-f = f_full(3: 122);
-ma_4_t = t_full(3: 122);
-
-b1 = f_full(1: 120);
-b2 = f_full(2: 121);
-b3 = f_full(4: 123);
-b4 = f_full(5: 124);
+% MA(4)
+fz = f_full(3: l-2);
+b1 = f_full(4: l-1);
+b2 = f_full(5: l);
+b3 = f_full(2: l-3);
+b4 = f_full(1: l-4);
 
 B = [b1; b2; b3; b4];
+c = (fz * B') * (B * B')^(-1);
+ma4_adapt = c*B;
+fprintf("koeficienty ma4: %d\n", c)
 
-c = (f*B')*(B*B')^(-1);
-ma_4_std = c*B;
+% MA(6)
+fz = f_full(4: l-3);
+b1 = f_full(5: l-2);
+b2 = f_full(6: l-1);
+b3 = f_full(7: l);
+b4 = f_full(3: l-4);
+b5 = f_full(2: l-5);
+b6 = f_full(1: l-6);
 
-% -------------------- MA(4) --------------------
-% f = f_full(3: l-2);
-% l = length(f);
-% ma_4_t = t_full(3: l-2);
-% 
-% b1 = f_full(2: l-3);
-% b2 = f_full(1: l-4);
-% b3 = f_full(4: l-1);
-% b4 = f_full(5: l);
-% B = [b1; b2; b3; b4];
-% 
-% c2 = (f*B')*(B*B')^(-1);
-% ma_4_std = c2*B;
+B = [b1; b2; b3; b4; b5; b6];
+c = (fz * B') * (B * B')^(-1);
+ma6_adapt = c*B;
+fprintf("koeficienty ma6: %d\n", c)
 
-% -------------------- MA(6) --------------------
-% f = f_full(6: l-6);
-% ma_6_t = t_full(6: l-6);
-% 
-% b1 = f_full(5: l-7);
-% b2 = f_full(4: l-8);
-% b3 = f_full(3: l-9);
-% b4 = f_full(7: l-5);
-% b5 = f_full(8: l-4);
-% b6 = f_full(9: l-3);
-% B = [b1; b2; b3; b4];
-% 
-% c = (f*B')*(B*B')^(-1);
-% ma_6_std = c*B;
+figure; hold on; grid on; xlim([1 124]); legend; title("Filtrácia pomocou adaptabilnej metódy MA(2)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, DisplayName="Dáta");
+plot(t_full(2:l-1), ma2_adapt, LineWidth=1.5, DisplayName="Adaptabilná MA(2)");
 
+figure; hold on; grid on; xlim([1 124]); legend; title("Filtrácia pomocou adaptabilnej metódy MA(4)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, DisplayName="Dáta");
+plot(t_full(2:l-1), ma2_adapt, DisplayName="Adaptabilná MA(2)");
+plot(t_full(3:l-2), ma4_adapt, LineWidth=1.5, DisplayName="Adaptabilná MA(4)");
 
-figure; hold on; grid on; xlim([1, 124]); legend;
-plot(t_full, f_full, '-', DisplayName="Povodne data");
-plot(ma_2_t, ma_2_std, '-', LineWidth=1.5,DisplayName="Standardna metoda MA(2)");
+figure; hold on; grid on; xlim([1 124]); legend; title("Filtrácia pomocou adaptabilnej metódy MA(6)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full, DisplayName="Dáta");
+plot(t_full(2:l-1), ma2_adapt, DisplayName="Adaptabilná MA(2)");
+plot(t_full(3:l-2), ma4_adapt, DisplayName="Adaptabilná MA(4)");
+plot(t_full(4:l-3), ma6_adapt, LineWidth=1.5, DisplayName="Adaptabilná MA(6)");
 
-figure; hold on; grid on; xlim([1, 124]); legend;
-plot(t_full, f_full, '-', DisplayName="Povodne data");
-plot(ma_2_t, ma_2_std,'--', DisplayName="Standardna metoda MA(2)");
-plot(ma_4_t, ma_4_std,'-', LineWidth=1.5, DisplayName="Standardna metoda MA(4)");
+% ---------- MA spolocne ---------- 
 
-% figure; hold on; grid on; xlim([1, 124]); legend;
-% plot(t_full, f_full, '-', DisplayName="Povodne data");
-% plot(ma_2_t, ma_2_std,'--', DisplayName="Standardna metoda MA(2)");
-% plot(ma_4_t, ma_4_std,'--', DisplayName="Standardna metoda MA(4)");
-% plot(ma_6_t, ma_6_std,'-', LineWidth=1.5, DisplayName="Standardna metoda MA(6)");
+figure; hold on; grid on; xlim([1 124]); legend; title("Porovnanie štandardnej a adaptabilnej metódy MA(2)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full)
+plot(t_full, ma2_std, LineWidth=1.5, DisplayName="Štandardná MA(2)");
+plot(t_full(2:l-1), ma2_adapt, LineWidth=1.5, DisplayName="Adaptabilná MA(2)");
 
+figure; hold on; grid on; xlim([1 124]); legend; title("Porovnanie štandardnej a adaptabilnej metódy MA(4)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full)
+plot(t_full, ma4_std, LineWidth=1.5, DisplayName="Štandardná MA(4)");
+plot(t_full(3:l-2), ma4_adapt, LineWidth=1.5, DisplayName="Adaptabilná MA(4)");
 
+figure; hold on; grid on; xlim([1 124]); legend; title("Porovnanie štandardnej a adaptabilnej metódy MA(6)"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+plot(t_full, f_full)
+plot(t_full, ma6_std, LineWidth=1.5, DisplayName="Štandardná MA(6)");
+plot(t_full(4:l-3), ma6_adapt, LineWidth=1.5, DisplayName="Adaptabilná MA(6)");
 
+% ----------------------- AR(1) ---------------------------
+okno = [5 10 15 20];
+c = {};
+format short;
+
+index_listu = 1;
+for n = okno
+    fz = f_full(1: end-n+1);
+    b = f_full(n: end);
+    c{index_listu} = (fz * b') / (b * b');
+
+    ar1{index_listu} = c{index_listu} * b;
+
+    index_listu = index_listu + 1;
+end	
+
+for n = 1:index_listu-1
+    figure; hold on; grid on; xlim([1 124]); legend; title(sprintf("Kĺzavý autoregresný koeficient AR(1) pre veľkosť okna %d", okno(n))); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+    plot(t_full, f_full, DisplayName="Dáta");
+    plot(t_full(okno(n): end), ar1{n}, LineWidth=1.5, DisplayName=sprintf("Dáta * koeficient %.4f", c{n}));
+end
 
 % ----------------------- exportovanie obrazkov ---------------------------
 resolution = 300;
@@ -112,18 +153,18 @@ figHandles = findall(groot, 'Type', 'figure');  % list of all open figure handle
 figHandles = flip(figHandles);
 
 % Loop through each figure and save it as a PNG
-for i = 1:length(figHandles)
-    fig = figHandles(i);
+for n = 1:length(figHandles)
+    fig = figHandles(n);
     
     % Set the figure's PaperPosition (this controls the output size)
     fig.PaperUnits = 'inches';
     fig.PaperPosition = [0 0 width height]; % [left bottom width height]
     
     % Create the filename with figure number
-    filename = sprintf('zadanie6obrazky/figure_%d.png', i); 
+    filename = sprintf('zadanie6obrazky/figure_%d.png', n); 
     
     % Save the figure as PNG with specified resolution
     print(fig, filename, '-dpng', ['-r', num2str(resolution)]);
 end
 
-close all
+close all;
