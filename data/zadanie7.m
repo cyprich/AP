51fc8949eeff9f7f0cc674a3c69fb7c4942d9b4a
cyprich <cypooriginal@gmail.com>
2024-12-20 22:37:46 +0100

    zadanie 7
    ani nemozem napisat ze zadanie 7 done lebo fakt ze nie je done ale uz nemam silu na to :))))) totalne ten predmet uz v zivote nechcem vidiet, dakujem dovidenia

--- data/zadanie7.m
+++ data/zadanie7.m
@@ -6,24 +6,25 @@ F = reshape(f_full(1:121), 11, 11);  % premeni data na 11x11 maticu
 F = (F') - mean((F'));  % vycentrovana a transponovana matica
 
 figure; hold on; grid on; legend; xlim([1 121]); title("Dáta - hodnoty akcií spoločnosti EA"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]"); 
-plot(t_full, f_full, DisplayName="Povodne data"); 
+plot(t_full, f_full, DisplayName="Pôvodné dáta"); 
 
-figure; hold on; grid on; legend(Location="northeastoutside"); title("Rozdelene data"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]"); xlim([1 121]); 
+figure; hold on; grid on; legend(Location="northeastoutside"); title("Rozdelené dáta"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]"); xlim([1 121]); 
 for i = 1:11
-    plot(linspace(i*11-10, i*11, 11), f_full(i*11-10: i*11), LineWidth=1.25, DisplayName=sprintf("%d. cast dat", i))
+    plot(linspace(i*11-10, i*11, 11), f_full(i*11-10: i*11), LineWidth=1.25, DisplayName=sprintf("%d. časť dát", i))
     xline(i*11, "--", HandleVisibility="off");
 end
 
-figure; hold on; grid on; legend(Location="northeastoutside"); title("Rozdelene data v jednom grafe"); xlabel("Počet dní od 1. 1. 2024"); ylabel("Hodnota akcie [$]");
+figure; hold on; grid on; legend(Location="northeastoutside"); title("Rozdelené dáta v jednom grafe");
 for i = 1:11
-    plot(F(i, :), DisplayName=sprintf("%d. cast dat", i));
+    plot(F(i, :), DisplayName=sprintf("%d. časť dát", i));
 end
 
 % ------------------ kovariancna matica --------------------
 R = cov(F);
+writematrix(R, "zadanie7_kovariancna_matica.xlsx")
 
 % for i = 1:30
-%     figure; hold on; title("Kovariancna matica"); xlabel("X"); ylabel("Y"), zlabel("Z")
+%     figure; hold on; title("Kovariančná matica"); xlabel("X"); ylabel("Y"), zlabel("Z")
 %     surf(R);
 %     view(i*12, 45);
 % end
@@ -32,7 +33,7 @@ R = cov(F);
 [U, S, V] = svd(R);
 vlastne_cisla = diag(S);
 
-figure, grid on; hold on; title("Spektrum vlastnych cisel")
+figure, grid on; hold on; title("Spektrum vlastných čísel")
 plot(vlastne_cisla);
 
 % ---------------- karhunen-loeve baza vlastnych vektorov ----------------
@@ -52,30 +53,23 @@ info1 = (vlastne_cisla(1) + vlastne_cisla(2)) / sum(vlastne_cisla);
 info2 = (vlastne_cisla(1) + vlastne_cisla(3)) / sum(vlastne_cisla);
 info3 = (vlastne_cisla(2) + vlastne_cisla(3)) / sum(vlastne_cisla);
 
+% ---------------- najpodobnejsie a najvzdialenejsie spektra -------------
 
+% Výpočet euklidovských vzdialeností medzi spektrami
+n = size(C, 2);
 
+for i = 1:n
+    for j = 1:n
+        vzdialenosti(i, j) = norm(C(:, i) - C(:, j));
+    end
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% vypocet min/max vzdialenosti
+[min_vzdialenost, min_i] = min(vzdialenosti(vzdialenosti > 0));
+[max_vzdialenost, max_i] = max(vzdialenosti(:)); 
+% indexy min/max oblasti
+[min_1, min_2] = ind2sub([n n], find(vzdialenosti == min_vzdialenost, 1));
+[max_1, max_2] = ind2sub([n n], find(vzdialenosti == max_vzdialenost, 1));
 
 
 
@@ -102,4 +96,4 @@ for n = 1:length(figHandles)
     print(fig, filename, '-dpng', ['-r', num2str(resolution)]);
 end
 
-% close all;
+close all;
